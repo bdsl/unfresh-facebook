df19e2c50b672b3d068c85049c8af4bf98963a99
Barney Laurance <barney@redmagic.org.uk>
2016-11-20 19:16:57 +0000

    Basic proof of concept - color code fresh content.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -5,9 +5,36 @@ chrome.extension.sendMessage({}, function(response) {
 
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
+		console.log("Unfresh Facebook by Barney Laurance is active");
 		// ----------------------------------------------------------
+                //
+                //
+                hideContent();
 
 	}
 	}, 10);
 });
+
+function hideContent(){
+    var cutoff = 1479667109;
+    cutoff = cutoff - 60*60*24;
+
+    hideNewContent(cutoff);
+}
+
+/**
+ * int cutoff - unix timestamp
+ * Remove facebook content published after cutoff
+ */
+function hideNewContent(cutoff) {
+    jQuery("div").each(function( index, element ) {
+       e = jQuery(element);
+       if (parseInt(e.data('timestamp')) > cutoff){
+           e.css({"border-color": "red", 
+                        "border-width":"1px", 
+                        "border-style":"solid"});
+       } else {
+       }
+
+   });
+}
